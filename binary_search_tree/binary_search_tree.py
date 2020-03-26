ad819a858209810b18195562b85ac893269d5775
Krunal Patel <Pkrunal616@gmail.com>
2020-03-25 21:09:42 -0400

    MPV done

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
 # import sys
 # sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
+# from dll_queue import Queue
+# from dll_stack import Stack
 
 
 class BinarySearchTree:
@@ -12,21 +12,54 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+
+        if self.value == None:
+            self.value = value
+        else:
+            if value < self.value:
+                if self.left is not None:
+                    self.left.insert(value)
+                else:
+                    self.left = BinarySearchTree(value)
+            else:
+                if self.right is not None:
+                    self.right.insert(value)
+                else:
+                    self.right = BinarySearchTree(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+
+        if target == self.value:
+            return True
+        elif target < self.value:
+            if self.left == None:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if self.right == None:
+                return False
+            else:
+                return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self.right is not None:
+            return self.right.get_max()
+        # else:
+        #     return False
+        return self.value
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        if self.left is not None:
+            self.left.for_each(cb)
+        if self.right is not None:
+            self.right.for_each(cb)
+        return cb(self.value)
 
     # DAY 2 Project -----------------------
 
